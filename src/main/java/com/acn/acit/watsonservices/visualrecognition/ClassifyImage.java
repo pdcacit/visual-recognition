dac65c55d8c808e998d4d9cac1583012b0683183
Fatima Hiprenoridel Hizon <fhchizon@gmail.com>
2018-04-03 16:50:26 +0800

    First commit

---
+++ src/main/java/com/acn/acit/watsonservices/visualrecognition/ClassifyImage.java
@@ -0,0 +1,21 @@
+package com.acn.acit.watsonservices.visualrecognition;
+
+public class ClassifyImage {
+	/*	
+	 * Please refer on Visual_Recognition_HandsOn.ppt
+	 * Visit API Reference https://www.ibm.com/watson/developercloud/visual-recognition/api/v3
+	 * Copy Maven dependency in pom.xml,
+	 * Update the pom dependency: Right click on project > Maven > Update Project
+	 * On API Reference: Click on JAVA tab.
+	 * On API Reference: Click on General > Classify Images
+	 * Copy and paste the code inside public static void main(String[] args){}
+	 * Import necessary libraries
+	 * Copy and change the api key based on your visual recognition service credentials.
+	 * To run using in eclipse: Right click on the project in eclipse > Run As > Java Application
+	 * To run using command prompt: Go to the project directory and type the command > 'mvn exec:java'
+	 * To rebuild type 'mvn package' or 'mvn verify' before executing 'mvn exec:java' command
+	*/
+	public static void main(String[] args){
+		//Paste code here...
+	}
+}
